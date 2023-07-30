7a14f703e83695cc8840fe433cfcdf6275d437cc
yentlvandamme <yentl.van.damme@gmail.com>
2023-07-30 02:07:24 +0200

    Setup default command structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{collections::HashMap, path::PathBuf};
+use clap::{Parser, Args, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    // The subcommand name which should be executed (add, remove, edit, ...)
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // Add alias to library
+    Add(DefaultArgs)
+}
+
+#[derive(Args)]
+struct DefaultArgs {
+    alias_name: String,
+    path: Option<String> // To be transformed to a PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add(args) => println!("ADDING, {}", args.alias_name), // Handle the logic to add
+                                                                        // a command
+    }
+    let mut aliases: HashMap<String, PathBuf> = HashMap::new();
 }
