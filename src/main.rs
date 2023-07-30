435d5d0e2eb43a33fd451a54afebd7367bb47366
yentlvandamme <yentl.van.damme@gmail.com>
2023-07-30 23:25:46 +0200

    Ability to add and print an alias

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,57 @@ use clap::{Parser, Args, Subcommand};
 
 #[derive(Parser)]
 struct Cli {
-    // The subcommand name which should be executed (add, remove, edit, ...)
     #[command(subcommand)]
     command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    // Add alias to library
-    Add(DefaultArgs)
+    Add(DefaultArgs),
+    Print
 }
 
 #[derive(Args)]
 struct DefaultArgs {
     alias_name: String,
-    path: Option<String> // To be transformed to a PathBuf
+    path: Option<String>
 }
 
 fn main() {
     let cli = Cli::parse();
+    let mut aliases = Aliases::init();
 
     match cli.command {
-        Commands::Add(args) => println!("ADDING, {}", args.alias_name), // Handle the logic to add
-                                                                        // a command
+        Commands::Add(args) => aliases.add(args.alias_name.clone()),
+        Commands::Print => aliases.all_aliases(),
     }
-    let mut aliases: HashMap<String, PathBuf> = HashMap::new();
 }
+
+pub struct Aliases {
+    aliases: HashMap<String, PathBuf>
+}
+
+impl Aliases {
+    pub fn init () -> Self {
+        Aliases{aliases: HashMap::new()}
+    }
+
+    pub fn add (&mut self, alias_name: String) {
+        self.aliases.insert(alias_name, PathBuf::new());
+    }
+
+    pub fn all_aliases (&self) {
+        let all_keys = self.aliases.keys();
+
+        for key in all_keys {
+            println!("{:?}", key);
+        }
+    }
+}
+
+// The idea is to build a struct which handles the aliases (storing, retrieving, updating,
+// removing)
+//
+// Down the line we'll implement additional commands such as moving, copying, removing, navigating
+// to, ... the file/folder the alias path is pointing to. For now we just want to make sure that we
+// can do CRUD operations
