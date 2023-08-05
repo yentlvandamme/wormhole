ecdccc106395ad5885c5e60feada05105f3f1afb
yentlvandamme <yentl.van.damme@gmail.com>
2023-08-05 16:41:38 +0200

    Allow adding aliases

---
+++ src/alias.rs
@@ -0,0 +1,67 @@
+use std::{collections::HashMap, path::PathBuf, env};
+
+pub struct Aliases {
+    aliases: HashMap<String, PathBuf>
+}
+
+impl Aliases {
+    pub fn init () -> Self {
+        Self {aliases: HashMap::new()}
+    }
+
+    pub fn add (&mut self, alias_name: String, path: Option<String>) {
+        let alias_path = match path {
+            Some(path) => PathBuf::from(path),
+            None => {
+                match env::current_dir() {
+                    Ok(location) => location,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        return
+                    }
+                }
+            },
+        };
+
+        self.aliases.insert(alias_name, alias_path);
+    }
+
+    pub fn all_aliases (&self) {
+        let all_keys = self.aliases.keys();
+
+        for key in all_keys {
+            println!("{:?}", key);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn add_valid_path() {
+        let mut alias_instance = Aliases::init();
+        let alias_name = String::from("TestName");
+        let alias_path = String::from("/bin");
+        let mut expected_hashmap_result = HashMap::new();
+
+        alias_instance.add(alias_name.clone(), Some(alias_path.clone()));
+        expected_hashmap_result.insert(alias_name.clone(), PathBuf::from(alias_path.clone()));
+
+        assert_eq!(expected_hashmap_result, alias_instance.aliases);
+    }
+
+    #[test]
+    fn add_alias_without_path() {
+        let mut alias_instance = Aliases::init();
+        let alias_name = String::from("TestNameFoo");
+        let current_dir = env::current_dir();
+        let mut expected_hashmap_result = HashMap::new();
+
+        alias_instance.add(alias_name.clone(), None);
+        expected_hashmap_result.insert(alias_name.clone(), current_dir.unwrap());
+
+        assert_eq!(expected_hashmap_result, alias_instance.aliases);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{collections::HashMap, path::PathBuf};
 use clap::{Parser, Args, Subcommand};
+use crate::alias::Aliases;
+
+pub mod alias;
 
 #[derive(Parser)]
 struct Cli {
@@ -24,36 +26,8 @@ fn main() {
     let mut aliases = Aliases::init();
 
     match cli.command {
-        Commands::Add(args) => aliases.add(args.alias_name.clone()),
+        Commands::Add(args) => aliases.add(args.alias_name, args.path),
         Commands::Print => aliases.all_aliases(),
     }
 }
 
-pub struct Aliases {
-    aliases: HashMap<String, PathBuf>
-}
-
-impl Aliases {
-    pub fn init () -> Self {
-        Aliases{aliases: HashMap::new()}
-    }
-
-    pub fn add (&mut self, alias_name: String) {
-        self.aliases.insert(alias_name, PathBuf::new());
-    }
-
-    pub fn all_aliases (&self) {
-        let all_keys = self.aliases.keys();
-
-        for key in all_keys {
-            println!("{:?}", key);
-        }
-    }
-}
-
-// The idea is to build a struct which handles the aliases (storing, retrieving, updating,
-// removing)
-//
-// Down the line we'll implement additional commands such as moving, copying, removing, navigating
-// to, ... the file/folder the alias path is pointing to. For now we just want to make sure that we
-// can do CRUD operations
